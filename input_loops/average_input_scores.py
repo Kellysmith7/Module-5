4cec7273bb6e6997f5ba0d31d2414f1aded889a8
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-21 14:32:23 -0500

    Passed Test

--- input_loops/average_input_scores.py
+++ input_loops/average_input_scores.py
@@ -5,5 +5,19 @@ Last date updated: 09/17/2019
 
 Program
 """
-def average(score_list):
-    pass
+
+
+def average():
+    scores =[]
+    score = (float(input("Enter a score or -1 to exit ")))
+    while score > 0:
+        scores.append(score)
+        score = (float(input("Enter a score or -1 to exit ")))
+    return sum(scores) / len(scores)
+
+
+if __name__ == '__main__':
+    first_name = input("Please enter first name ")
+    last_name = input("Please enter last name ")
+    age = input("Please enter age ")
+    print(last_name + ", " + first_name + " " + "Age: " + age + " " + "Average Score: % 5.2f" % average())
--- test/test_loops.py
+++ test/test_loops.py
@@ -4,7 +4,7 @@ from input_loops import average_input_scores
 
 class MyTestCase(unittest.TestCase):
     def test_average(self):
-        self.assertEqual(90, average_input_scores.average(90, 95, 85))
+        self.assertEqual(90, average_input_scores.average())
 
 
 if __name__ == '__main__':
